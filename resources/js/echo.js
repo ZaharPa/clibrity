0d4cd1022139c81d2488b1c7fae44fc1f22552ca
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2025-01-20 14:14:09 +0000

    second try WebSockets

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,17 +1,16 @@
 import Echo from 'laravel-echo';
+
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
+    broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT || 8080,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    encrypted: true,
     enabledTransports: ['ws', 'wss'],
-    cluster: 'mt1'
 });
 
 window.Echo.channel('posts')
